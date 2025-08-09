ce840167fe6e26cf5aa4f776a55433c97ed33ce9
NhanNT <nhannt.bk@gmail.com>
2025-08-10 01:03:15 +0900

    Update program id

--- programs/solid-svm/src/lib.rs
+++ programs/solid-svm/src/lib.rs
@@ -6,7 +6,7 @@ mod utils;
 
 use crate::handlers::*;
 
-declare_id!("65ggx1TjBoGrwzPtD77QmFugfzDxRz6TTDKrxTGni3Js");
+declare_id!("EkBnxBMuEm3etvxmGamRKjFva2TXKZ5qhxomN1PXNJS7");
 
 #[program]
 pub mod solid_svm {
